f157043b74b910864fcf3cfd81b0923f01b083bc
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-10 09:51:01 +0700

    adding po

--- ReportInvoicePage.js
+++ ReportInvoicePage.js
@@ -295,7 +295,40 @@ export const ReportInvoicePage = () => {
                 </Col>
               </Form.Group> */}
               <Form.Group as={Row}>
-                <Col sm={3}>
+                <Col sm={6}>
+                  {user.purch_org !== null && (
+                    <Form.Group as={Row} className="mt-3">
+                      <Form.Label column sm={6}>
+                        <b>Purchasing Organization</b>
+                      </Form.Label>
+                      <Col sm={6}>
+                        <Form.Control
+                          type="text"
+                          placeholder="Purchasing Organization"
+                          value={user.purch_org}
+                          disabled
+                        />
+                      </Col>
+                    </Form.Group>
+                  )}
+                  {user.purch_org === null && (
+                    <Form.Group as={Row} className="mt-3">
+                      <Form.Label column sm={6}>
+                        <b>Purchasing Organization</b>
+                      </Form.Label>
+                      <Col sm={6}>
+                        <Form.Control
+                          type="text"
+                          placeholder="Purchasing Organization"
+                          onChange={(e) => {
+                            setVendorCode(e.target.value); 
+                          }}
+                          value={vendorCode} 
+                          onKeyPress={handleKeyPress}
+                        />
+                      </Col>
+                    </Form.Group>
+                 )}
                   <Button className="btn btn-danger" onClick={handleSearch}>
                     Search
                   </Button>
