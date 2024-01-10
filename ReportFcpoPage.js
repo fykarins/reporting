86ce63f923b8ed4ee1cd422b7d471b4e1c2981d3
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-10 09:10:39 +0700

    Add files via upload

---
+++ ReportFcpoPage.js
@@ -0,0 +1,341 @@
+import React, { useEffect, useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardHeaderToolbar,
+} from "../../../../_metronic/_partials/controls";
+import { ReportFcpoTable } from "./ReportFcpoTable";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  resetData,
+  selectReporting,
+  selectLoading,
+  selectPageNo,
+  selectPageSize,
+  selectTotalRecord,
+  fetchReporting,
+  fetchFile,
+} from "../po/reportingSlice";
+import { LayoutSplashScreen } from "../../../../_metronic/layout";
+import { showErrorDialog } from "../../../utility";
+import { selectUser } from "../../../modules/Auth/_redux/authRedux";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import LoadingFetchData from "../../../utility/LoadingFetchData";
+
+export const ReportFcpoPage = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const data = useSelector(selectReporting);
+  const user = useSelector(selectUser);
+  const loading = useSelector(selectLoading);
+  const pageNo = useSelector(selectPageNo);
+  const pageSize = useSelector(selectPageSize);
+  const totalRecord = useSelector(selectTotalRecord);
+
+  // Filter
+  const [materialNumber, setMaterialNumber] = useState("");
+  const [vendorCode, setVendorCode] = useState("");
+  const [startPeriod, setStartPeriod] = useState(null);
+  const [endPeriod, setEndPeriod] = useState(null);
+  const [overlayLoading, setOverlayLoading] = useState(false);
+
+  const filterVendorCode =
+    user.vendor_code === null
+      ? vendorCode
+      : user.vendor_code.replace("0000", "");
+
+  useEffect(() => {
+    // Reset on first load
+    dispatch(resetData());
+  }, [dispatch]);
+
+  const handleSearch = async () => {
+    const params = {
+      reportType: "ForecastCampignvsPO",
+      Vendor_Code: filterVendorCode,
+      Material_Number: materialNumber,
+      start_Period: startPeriod,
+      end_Period: endPeriod,
+      pageNo: 1,
+      pageSize: 10,
+    };
+
+    console.log("params", params);
+
+    //--Validasi--//
+    if (startPeriod && endPeriod === null) {
+      return showErrorDialog("Please Select End Date ");
+    }
+    if (endPeriod && startPeriod === null) {
+      return showErrorDialog("Please Select Start Date");
+    }
+    //-- --//
+
+    try {
+      const response = await dispatch(fetchReporting(params));
+      if (response.payload.data.status === 200) {
+        console.log(response, "if");
+        setOverlayLoading(false);
+      } else if (
+        response.payload.data.error === "10008" ||
+        response.payload.data.error === "10009"
+      ) {
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+      } else {
+        console.log(response, "else");
+
+        showErrorDialog(response.payload.data.message);
+        setOverlayLoading(false);
+      }
+    } catch (error) {
+      showErrorDialog(error.message);
+      setOverlayLoading(false);
+    }
+  };
+
+  const handleTableChange = async (
+    type,
+    { page, sizePerPage, sortField, sortOrder, data }
+  ) => {
+    if (type === "pagination") {
+      const params = {
+        reportType: "ForecastCampignvsPO",
+        Vendor_Code: filterVendorCode,
+        Material_Number: materialNumber,
+        start_Period: startPeriod,
+        end_Period: endPeriod,
+        pageNo: page,
+        pageSize: sizePerPage,
+      };
+      try {
+        const response = await dispatch(fetchReporting(params));
+        if (response.payload.data.status === 200) {
+          setOverlayLoading(false);
+        } else if (
+          response.payload.data.error === "10008" ||
+          response.payload.data.error === "10009"
+        ) {
+          const action = await showErrorDialog(response.payload.data.message);
+          if (action.isConfirmed) await history.push("/logout");
+        } else {
+          showErrorDialog(response.payload.data.message);
+          setOverlayLoading(false);
+        }
+      } catch (error) {
+        showErrorDialog(error.message);
+        setOverlayLoading(false);
+      }
+    } else {
+      let result;
+      if (sortOrder === "asc") {
+        result = data.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return 1;
+          } else if (b[sortField] > a[sortField]) {
+            return -1;
+          }
+          return 0;
+        });
+        console.log(result, "asc");
+      } else {
+        result = data.sort((a, b) => {
+          if (a[sortField] > b[sortField]) {
+            return -1;
+          } else if (b[sortField] > a[sortField]) {
+            return 1;
+          }
+          return 0;
+        });
+        console.log(result, "desc");
+      }
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const handleDownload = async () => {
+    const params = {
+      reportType: "ForecastCampignvsPO",
+      Vendor_Code: filterVendorCode,
+      Material_Number: materialNumber,
+      startPeriod: startPeriod,
+      endtPeriod: endPeriod,
+    };
+    try {
+      const response = await dispatch(fetchFile(params));
+      if (response.payload.status === 200) {
+        const blob = new Blob([response.payload.data], {
+          type: "application/xlsx",
+        });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "Report_Forcast+Campaign_vs_Purchase_Order.xlsx";
+        link.click();
+      } else {
+        showErrorDialog(response.payload.data.message);
+        console.log("else");
+      }
+    } catch (error) {
+      console.log("catch");
+
+      showErrorDialog(error.message);
+    }
+  };
+
+  return loading ? (
+    <LayoutSplashScreen />
+  ) : (
+    <Card>
+      <CardHeader title="Report Forecast+Campaign vs PO">
+        <CardHeaderToolbar>
+          <Button className="btn btn-danger mr-3" onClick={handleDownload}>
+            Download Excel
+          </Button>
+        </CardHeaderToolbar>
+      </CardHeader>
+      <LoadingFetchData active={overlayLoading} />
+      <CardBody>
+        {/* Filter */}
+        <div className="mb-5">
+          <Form.Group as={Row}>
+            <Col sm={6}>
+              {user.vendor_code !== null && (
+                <Form.Group as={Row}>
+                  <Form.Label column sm={3}>
+                    <b>Vendor </b>
+                  </Form.Label>
+                  <Col sm={6}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Vendor"
+                      value={user.vendor_name}
+                      disabled
+                    />
+                  </Col>
+                </Form.Group>
+              )}
+
+              {user.vendor_code === null && (
+                <Form.Group as={Row}>
+                  <Form.Label column sm={3}>
+                    <b>Vendor</b>
+                  </Form.Label>
+                  <Col sm={6}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Vendor"
+                      onChange={(e) => {
+                        setVendorCode(e.target.value);
+                      }}
+                      value={vendorCode}
+                      onKeyPress={handleKeyPress}
+                    />
+                  </Col>
+                </Form.Group>
+              )}
+
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  <b>Material Number</b>
+                </Form.Label>
+                <Col sm={6}>
+                  <Form.Control
+                    type="text"
+                    placeholder="Material Number"
+                    onChange={(e) => {
+                      setMaterialNumber(e.target.value);
+                    }}
+                    value={materialNumber}
+                    onKeyPress={handleKeyPress}
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+
+            {/* Right Row */}
+            <Col sm={6}>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  <b>Period From</b>
+                </Form.Label>
+                <Col sm={8}>
+                  <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <DatePicker
+                      clearable
+                      autoOk
+                      views={["year", "month"]}
+                      format="YYYY-MM"
+                      emptyLabel="YYYY-MM"
+                      value={startPeriod}
+                      onChange={(e) => {
+                        if (e) {
+                          const date = e.format("YYYY-MM");
+                          setStartPeriod(date);
+                        } else {
+                          setStartPeriod(null);
+                        }
+                      }}
+                    />
+                  </MuiPickersUtilsProvider>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  <b>Period To</b>
+                </Form.Label>
+                <Col sm={8}>
+                  <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <DatePicker
+                      clearable
+                      autoOk
+                      views={["year", "month"]}
+                      format="YYYY-MM"
+                      emptyLabel="YYYY-MM"
+                      value={endPeriod}
+                      onChange={(e) => {
+                        if (e) {
+                          const date = e.format("YYYY-MM");
+                          setEndPeriod(date);
+                        } else {
+                          setEndPeriod(null);
+                        }
+                      }}
+                    />
+                  </MuiPickersUtilsProvider>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Col sm={3}>
+                  <Button className="btn btn-danger" onClick={handleSearch}>
+                    Search
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Col>
+          </Form.Group>
+        </div>
+
+        {/* Table */}
+        {data && data.reportData && data.reportData.length > 0 && (
+          <ReportFcpoTable
+            data={data.reportData}
+            page={pageNo}
+            sizePerPage={pageSize}
+            totalSize={totalRecord}
+            onTableChange={handleTableChange}
+            loading={loading}
+          />
+        )}
+      </CardBody>
+    </Card>
+  );
+};
