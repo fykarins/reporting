395640a508c9fc423fa38e8eb4271e89aab5b699
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-10 11:17:06 +0700

    connect api

--- ReportDealerRequestPage.js
+++ ReportDealerRequestPage.js
@@ -41,11 +41,14 @@ export const ReportDealerRequestPage = () => {
   const [startTglPengajuan, setStartTglPengajuan] = useState("");
   const [endTglPengajuan, setEndTglPengajuan] = useState("");
   const [overlayLoading, setOverlayLoading] = useState(false);
+  const [valueNmbr, setValueNmbr] = useState(""); //buat deklarasi state
 
   const filterVendorCode =
     user.vendor_code === null
       ? vendorCode
       : user.vendor_code.replace("0000", "");
+  const filterPurOrg =
+    user.purch_org === null ? valueNmbr : user.purch_org;
 
   useEffect(() => {
     // Reset on first load
@@ -66,6 +69,7 @@ export const ReportDealerRequestPage = () => {
       Kode_Dealer: kodeDealer,
       start_Tanggal_Pengajuan: startTglPengajuan,
       end_Tanggal_Pengajuan: endTglPengajuan,
+      purch_org: filterPurOrg, //valueNmbr, //parameter pembacaan u/ melakukan permintaan API
       pageNo: 1,
       pageSize: 10,
     };
@@ -111,6 +115,7 @@ export const ReportDealerRequestPage = () => {
         Kode_Dealer: kodeDealer,
         start_Tanggal_Pengajuan: startTglPengajuan,
         end_Tanggal_Pengajuan: endTglPengajuan,
+        purch_org: filterPurOrg, //valueNmbr, //parameter pembacaan u/ melakukan permintaan API
         pageNo: page,
         pageSize: sizePerPage,
       };
@@ -122,10 +127,14 @@ export const ReportDealerRequestPage = () => {
           response.payload.data.error === "10008" ||
           response.payload.data.error === "10009"
         ) {
+          // Corrected the syntax here
           const action = await showErrorDialog(response.payload.data.message);
           if (action.isConfirmed) await history.push("/logout");
         } else {
-          showErrorDialog(response.payload.data.message);
+          // Corrected the syntax here
+          const action = await showErrorDialog(response.payload.data.message);
+          if (action.isConfirmed) await history.push("/logout");
+          valueNmbr = action.payload.value; // Corrected the syntax here
           setOverlayLoading(false);
         }
       } catch (error) {
@@ -326,9 +335,9 @@ export const ReportDealerRequestPage = () => {
                           type="text"
                           placeholder="Purchasing Organization"
                           onChange={(e) => {
-                            setVendorCode(e.target.value); 
+                            setValueNmbr(e.target.value); 
                           }}
-                          value={vendorCode} 
+                          value={valueNmbr} 
                           onKeyPress={handleKeyPress}
                         />
                       </Col>
