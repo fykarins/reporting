3b75c5a83c6fc74b2e3570eef5e4a94aca872d9e
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-11 08:00:46 +0700

    api

--- ReportSipPage.js
+++ ReportSipPage.js
@@ -75,10 +75,14 @@ export const ReportSipPage = () => {
         response.payload.data.error === "10008" ||
         response.payload.data.error === "10009"
       ) {
+        // Corrected the syntax here
         const action = await showErrorDialog(response.payload.data.message);
         if (action.isConfirmed) await history.push("/logout");
       } else {
-        showErrorDialog(response.payload.data.message);
+        // Corrected the syntax here
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+        valueNmbr = action.payload.value; // Corrected the syntax here
         setOverlayLoading(false);
       }
     } catch (error) {
@@ -153,6 +157,7 @@ export const ReportSipPage = () => {
       reportType: "sip",
       Vendor: filterVendorCode,
       PO_Number: noPo,
+      purch_org: filterPurOrg, //valueNmbr, //parameter pembacaan u/ melakukan permintaan API
     };
     try {
       const response = await dispatch(fetchFile(params));
@@ -160,12 +165,16 @@ export const ReportSipPage = () => {
         const blob = new Blob([response.payload.data], {
           type: "application/xlsx",
         });
+        // Corrected the syntax here
         const link = document.createElement("a");
         link.href = window.URL.createObjectURL(blob);
         link.download = "Report_Surat_Instruksi_Pengiriman.xlsx";
         link.click();
       } else {
-        showErrorDialog(response.payload.data.message);
+        // Corrected the syntax here
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+        valueNmbr = action.payload.value; // Corrected the syntax here
         console.log("else");
       }
     } catch (error) {
