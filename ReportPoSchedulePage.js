8109a7c1e1c2a5e6f32bead826a74806c3a9204c
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-11 07:53:48 +0700

    api

--- ReportPoSchedulePage.js
+++ ReportPoSchedulePage.js
@@ -79,10 +79,14 @@ export const ReportPoSchedulePage = () => {
         response.payload.data.error === "10008" ||
         response.payload.data.error === "10009"
       ) {
+        // Corrected the syntax here
         const action = await showErrorDialog(response.payload.data.message);
         if (action.isConfirmed) await history.push("/logout");
       } else {
-        showErrorDialog(response.payload.data.message);
+        // Corrected the syntax here
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+        valueNmbr = action.payload.value; // Corrected the syntax here
         setOverlayLoading(false);
       }
     } catch (error) {
@@ -161,6 +165,7 @@ export const ReportPoSchedulePage = () => {
       Vendor_Name: vendorName,
       PO_DATE: poDate,
       PO_Number: poNumber,
+      purch_org: filterPurOrg, //valueNmbr, //parameter pembacaan u/ melakukan permintaan API
     };
     try {
       const response = await dispatch(fetchFile(params));
@@ -168,12 +173,16 @@ export const ReportPoSchedulePage = () => {
         const blob = new Blob([response.payload.data], {
           type: "application/xlsx",
         });
+        // Corrected the syntax here
         const link = document.createElement("a");
         link.href = window.URL.createObjectURL(blob);
         link.download = "Report_Purchase_Order_Delivery_Schedule.xlsx";
         link.click();
       } else {
-        showErrorDialog(response.payload.data.message);
+        // Corrected the syntax here
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+        valueNmbr = action.payload.value; // Corrected the syntax here
         console.log("else");
       }
     } catch (error) {
