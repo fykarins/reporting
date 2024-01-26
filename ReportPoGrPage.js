af619cafa04c87104193837511e6a3094ce5b336
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-26 14:32:54 +0700

    rapikan

--- ReportPoGrPage.js
+++ ReportPoGrPage.js
@@ -317,37 +317,35 @@ export const ReportPoGrPage = () => {
                   />
                 </Col>
               </Form.Group>
-              <Form.Group as={Row}>
-                <Col sm={6}>
-                  {user.purch_org !== null && (
-                    <Form.Group as={Row} className="mt-3">
-                      <Form.Label column sm={6}>
-                        <b>Purchasing Organization</b>
-                      </Form.Label>
-                      <Col sm={6}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Purchasing Organization"
-                          value={user.purch_org}
-                          disabled
-                        />
-                      </Col>
-                    </Form.Group>
-                  )}
-                  {user.purch_org === null && (
-                    <Form.Group as={Row} className="mt-3">
-                      <Form.Label column sm={6}>
-                        <b>Purchasing Organization</b>
-                      </Form.Label>
-                      <Col sm={6}>
-                        <Form.Control
-                          type="text"
-                          placeholder="Purchasing Organization"
-                          onChange={(e) => {
-                            setValueNmbr(e.target.value);
-                          }}
-                          value={valueNmbr} 
-                          onKeyPress={handleKeyPress}
+              {user.purch_org !== null && (
+                <Form.Group as={Row} className="mt-3">
+                  <Form.Label column sm={3}>
+                    <b>Purchasing Organization</b>
+                  </Form.Label>
+                    <Col sm={6}>
+                      <Form.Control
+                        type="text"
+                        placeholder="Purchasing Organization"
+                        value={user.purch_org}
+                        disabled
+                      />
+                    </Col>
+                </Form.Group>
+              )}
+              {user.purch_org === null && (
+                <Form.Group as={Row} className="mt-3">
+                  <Form.Label column sm={3}>
+                    <b>Purchasing Organization</b>
+                  </Form.Label>
+                    <Col sm={6}>
+                      <Form.Control
+                        type="text"
+                        placeholder="Purchasing Organization"
+                        onChange={(e) => {
+                          setValueNmbr(e.target.value);
+                        }}
+                        value={valueNmbr} 
+                        onKeyPress={handleKeyPress}
                         />
                       </Col>
                     </Form.Group>
@@ -355,7 +353,6 @@ export const ReportPoGrPage = () => {
                   <Button className="btn btn-danger" onClick={handleSearch}>
                     Search
                   </Button>
-                </Col>
                 {/* <Col sm={3}>
                   <Button
                     className="btn btn-danger"
@@ -364,7 +361,6 @@ export const ReportPoGrPage = () => {
                     Create
                   </Button>
                 </Col> */}
-              </Form.Group>
             </Col>
           </Form.Group>
         </div>
